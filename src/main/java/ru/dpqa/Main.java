8d612e54a8c4339698154d1eb7c0b721f10b3bab
Daria Petrova <38262231+da-shutka@users.noreply.github.com>
2024-04-16 00:03:03 +0300

    Message to variable

--- src/main/java/ru/dpqa/Main.java
+++ src/main/java/ru/dpqa/Main.java
@@ -1,7 +1,9 @@
 package ru.dpqa;
 
 public class Main {
+    private statis String msg = "Hello and bye-bye QA Guru! Ha-ha-ha!";
+    
     public static void main(String[] args) {
-        System.out.println("Hello and bye-bye QA Guru! Ha-ha-ha!");
+        System.out.println(msg);
     }
 }
