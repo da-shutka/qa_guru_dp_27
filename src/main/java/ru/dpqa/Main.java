49740b370da74343679b3d232273332599cf2af7
Daria Petrova <petrova.tpu@gmail.com>
2024-04-16 00:03:59 +0300

    Added getMessage() method

--- src/main/java/ru/dpqa/Main.java
+++ src/main/java/ru/dpqa/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello QA Guru!");
     }
+
+    private static String getMessage() {
+        return "Hello new QA Guru!";
+    }
 }
