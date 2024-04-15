fb1c10444450e49d20fd24674080f2c9f5128100
Daria Petrova <38262231+da-shutka@users.noreply.github.com>
2024-04-16 00:01:38 +0300

    Updated funny hello text

--- src/main/java/ru/dpqa/Main.java
+++ src/main/java/ru/dpqa/Main.java
@@ -2,6 +2,6 @@ package ru.dpqa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello QA Guru!");
+        System.out.println("Hello and bye-bye QA Guru! Ha-ha-ha!");
     }
 }
